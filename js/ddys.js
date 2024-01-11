2676af14c55680107dc0fcc788f72c4a61127c83
namegenliang <156243938+namegenliang@users.noreply.github.com>
2024-01-11 14:30:00 +0800

    Add files via upload

--- js/ddys.js
+++ js/ddys.js
@@ -151,7 +151,11 @@ let d = pdfa(html, 'div.wp-playlist~a');
 let tabsq=[];
 d.forEach(function(it){
 	let burl = pd(it, 'a&&href', HOST);
-	
+	if (burl.includes("pan.quark.cn")){
+		tabsq.push("夸克網盤");
+	}else if (burl.includes("www.aliyundrive.com") || burl.includes("www.alipan.com")){
+		tabsq.push("阿里雲盤");
+	}
 });
 if (tabsq.length == 1){
 	TABS=TABS.concat(tabsq);
