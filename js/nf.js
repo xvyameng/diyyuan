c6d6401a2a41465bfb808f6b37a385f6642aa2db
namegenliang <156243938+namegenliang@users.noreply.github.com>
2024-01-31 15:20:11 +0800

    Update nf.js

--- js/nf.js
+++ js/nf.js
@@ -7,20 +7,17 @@
             searchable: 2,//是否启用全局搜索,
             quickSearch: 0,//是否启用快速搜索,
             filterable: 0,//是否启用分类筛选,
-            class_parse: '.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-            play_parse: true,
-            lazy: '',
-            limit: 6,
-            推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-            double: true, // 推荐内容是否双层定位
-            一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-            二级: {
-                "title": "h1&&Text;.tag-link&&Text",
-                "img": ".module-item-pic&&img&&data-src",
-                "desc": ".video-info-aux&&.tag-link&&Text;.video-info-aux&&a:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(5)&&Text",
-                "content": ".vod_content&&Text",
-                "tabs": ".module-tab-item",
-                "lists": ".module-player-list:eq(#id)&&.scroll-content&&a"
-            },
-            搜索: '.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
-        }
+headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+class_parse:'.navbar li;a&&Text;a&&href;/(\\d+).html',
+play_parse:true,
+lazy:'',
+limit:6,
+推荐:'.module;.module-main;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+double:true, // 推荐内容是否双层定位
+一级:'.module .module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":";;.module-info-main&&.module-info-item:eq(1)&&Text;.module-info-main&&.module-info-item:eq(2)&&Text","content":".show-desc&&Text","tabs":"#y-playList&&.tab-item","lists":".module-play-list-content:eq(#id) a"},
+搜索:'.module&&.module-main;.lazy&&alt;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+}
